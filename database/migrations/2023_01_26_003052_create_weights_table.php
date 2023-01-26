2d2e0598e9344a8c7e8e0b1ba0e752ffe28e020e
shvalin1 <shvalin01@gmail.com>
2023-01-26 09:39:24 +0900

    [edit] weights migration_fileの編集 #10

--- database/migrations/2023_01_26_003052_create_weights_table.php
+++ database/migrations/2023_01_26_003052_create_weights_table.php
@@ -15,6 +15,9 @@ return new class extends Migration
     {
         Schema::create('weights', function (Blueprint $table) {
             $table->id();
+            $table->foreignId('user_id')->nullable()->constrained()->cascadeOnDelete();
+            $table->float("weight");
+            $table->date("measure_at");
             $table->timestamps();
         });
     }
@@ -26,6 +29,11 @@ return new class extends Migration
      */
     public function down()
     {
+        // userテーブルとの連携を解除してweightsテーブルを削除
+        Schema::table('weights', function (Blueprint $table) {
+            $table->dropForeign(['user_id']);       // userテーブルとの連携を解除
+            $table->dropColumn(['user_id']);        // user_idカラムを削除
+        });
         Schema::dropIfExists('weights');
     }
 };
