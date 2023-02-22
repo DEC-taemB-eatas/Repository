caae2140d504959cd4c57602c3d4d2b93a6b9411
shvalin1 <shvalin01@gmail.com>
2023-02-22 14:46:33 +0900

    [edit]seederファイルを編集 #54

--- database/seeders/TargetSeeder.php
+++ database/seeders/TargetSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\Target;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -14,6 +15,15 @@ class TargetSeeder extends Seeder
      */
     public function run()
     {
-        //
+        $this->command->info("目標データの作成を開始します...");
+
+        $json = file_get_contents(__DIR__ . '/data/target.json');
+        $targets = json_decode($json, true);
+
+        foreach ($targets['targets'] as $target) {
+            Target::create($target);
+        }
+
+        $this->command->info("目標データを作成しました。");
     }
 }
