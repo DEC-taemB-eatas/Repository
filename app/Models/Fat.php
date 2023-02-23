97780b6454a6ff0b27690c87e1379be3f22ffe28
shvalin1 <shvalin01@gmail.com>
2023-02-23 12:46:33 +0900

    [edit]Fatモデルに関数を追加 #64

--- app/Models/Fat.php
+++ app/Models/Fat.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\Auth;
 
 class Fat extends Model
 {
@@ -13,4 +14,20 @@ class Fat extends Model
         'created_at',
         'updated_at',
     ];
+
+    public function fat():Array
+        {
+            $fats = User::query()
+                ->find(Auth::user()->id)
+                ->userFats()
+                ->orderByDesc('measure_at')
+                ->get();
+
+            //順番に配列に代入
+            foreach($fats as $fat){
+                $fat_lis[] = $fat -> fat;
+            }
+
+            return $fat_lis;
+        }
 }
