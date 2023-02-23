c7cfb80f06527ef446bd683f11cdaa11f0f6d05d
shvalin1 <shvalin01@gmail.com>
2023-02-23 13:42:24 +0900

    [edit]Targetモデルに関数を追加 #59

--- app/Models/Target.php
+++ app/Models/Target.php
@@ -16,4 +16,25 @@ class Target extends Model
         'created_at',
         'updated_at',
     ];
+
+    public function getTargetList($type):Array
+    {
+        $targets = Target::query()
+            ->whereType(0)//本当はここにタイプの変数が入る
+            ->orderByDesc('term')
+            ->get();
+
+        //順番に配列に代入
+        foreach($targets as $target){
+            $data_lis[] = $target -> bmi;
+            $date_lis[] = $target -> term;
+        }
+
+        $data = array(
+            'data' => $data_lis,
+            'date' => $date_lis
+        );
+
+        return $data;
+    }
 }
