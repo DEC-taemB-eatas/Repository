9b1a961cebdce04f9303e87baf293d0ad883866a
shvalin1 <shvalin01@gmail.com>
2023-02-26 14:49:40 +0900

    [edit]体重データの入力期間を変数化 #104

--- app/Http/Controllers/BodyController.php
+++ app/Http/Controllers/BodyController.php
@@ -35,9 +35,11 @@ class BodyController extends Controller
     public function index()
     {
         $fats = $this -> fat -> getFatList();
-        $weights = $this -> weight -> getWeightList();        
+        $weights = $this -> weight -> getWeightList();     
+        $length = count($weights['date']);
+
         $muscles = $this -> muscle -> getMuscleList();
-        $targets = $this -> target -> getTargetList();//本当はここにタイプを入れる
+        $targets = $this -> target -> getTargetList($length);
         $comments = $this -> comment -> getComment();
         $scores = $this -> score -> getScore();
 
