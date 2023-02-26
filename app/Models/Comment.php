1a87e242f4382efed4fc9c7ff9855d7d9756239d
shvalin1 <shvalin01@gmail.com>
2023-02-26 13:06:11 +0900

    [edit]コメントの場合分けを実装

--- app/Models/Comment.php
+++ app/Models/Comment.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\Auth;
 
 class Comment extends Model
 {
@@ -17,13 +18,40 @@ class Comment extends Model
 
     public function getComment(){
 
-        $type = 1;//これは質問の点数で判定
+        $scores = User::query()
+        ->find(Auth::user()->id)
+        ->userScores()
+        ->first();
 
+        $data = array(
+            1 => 80, //$scores -> eating,
+            2 => 40,//$scores -> eating_habits,
+            3 => 30,//$scores -> ability_to_act,
+            4 => 20,//$scores -> physical_condition
+        );
+       
+        //dd(array_keys($data,max($data)));
+
+        if (max($data)>=80){
+            $comment_id[]=array_keys($data,max($data))[0];
+            $comment_id[]=array_keys($data,min($data))[0]+4;
+        }else{
+            $comment_id[]=array_keys($data,min($data))[0]+4;
+            unset($data[$comment_id[0]-4]);
+            $comment_id[]=array_keys($data,min($data))[0]+4;
+        };
+         
         $comments = Comment::query()
-            ->find($type)
-            ->first();
+            ->whereIn('id',$comment_id)
+            ->get();
+    
+        //dd($comments);
+        foreach($comments as $comment){
+            $comment_lis[]=$comment->comment;
+        }
 
-        $comment = $comments -> comment;
-        return $comment;
+        $result = join('。',$comment_lis);
+        
+        return $result;
     }
 }
