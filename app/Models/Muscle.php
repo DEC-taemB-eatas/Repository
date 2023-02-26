30715477fc3103aa602a87cc4460ead46a940a8a
shvalin1 <shvalin01@gmail.com>
2023-02-26 10:56:59 +0900

    [edit]muscleモデルにも線形補間を実装 #100

--- app/Models/Muscle.php
+++ app/Models/Muscle.php
@@ -25,15 +25,37 @@ class Muscle extends Model
             ->get();
 
         //順番に配列に代入
-            foreach($muscles as $muscle){
+        foreach($muscles as $muscle){
                 $data_lis[] = $muscle -> muscle;
                 $date_lis[] =new Carbon($muscle -> measure_at);//日付計算を行うため、Carbon形式で配列に入れる
             }
 
-            foreach($date_lis as $date){
+        foreach($date_lis as $date){
                 $date_lis_diff[] = $date -> diffInDays($date_lis[0]);//測定開始日との差分を計算する
             }
             
+        $data_lis_new[] = $data_lis[0];
+        $date_lis_new[] = $date_lis_diff[0];
+
+        for ($i = 1; $i < count($data_lis); $i++){
+            $date_diff = $date_lis_diff[$i] - $date_lis_diff[$i - 1];
+            $data_diff = $data_lis[$i] - $data_lis[$i - 1];
+            $date_diff_lis[]=$date_diff;
+            if($date_diff != 1){
+                for ($j = 1;$j <= $date_diff ; $j++){
+                    $date_lis_new[] = $date_lis_diff[$i - 1] + $j;
+                    $data_lis_new[] = $data_lis[$i - 1] + ($data_diff * $j / $date_diff);
+                }
+            }else{
+                $data_lis_new[] = $data_lis[$i];
+                $date_lis_new[] = $date_lis_diff[$i];
+                }
+            }
+
+        $data = array(
+            'data' => $data_lis_new,
+            'date' => $date_lis_new
+        );
             $data = array(
                 'data' => $data_lis,
                 'date' => $date_lis_diff
